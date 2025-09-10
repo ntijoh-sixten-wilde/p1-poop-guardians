dbab263a9943dc1681259c39d6e569d5f7634920
EirikNilsson <eirik.haugennilsson@elev.ga.ntig.se>
2025-09-10 14:33:38 +0200

    Small fixes

--- lib/mix/seed.ex
+++ lib/mix/seed.ex
@@ -11,6 +11,9 @@ defmodule Mix.Tasks.Seed do
 
   defp drop_tables() do
     IO.puts("Dropping tables")
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS ingredients", [], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS users", [], pool: DBConnection.ConnectionPool)
+    Postgrex.query!(DB, "DROP TABLE IF EXISTS fruits", [], pool: DBConnection.ConnectionPool)
     Postgrex.query!(DB, "DROP TABLE IF EXISTS pizzas", [], pool: DBConnection.ConnectionPool)
     Postgrex.query!(DB, "DROP TABLE IF EXISTS orders", [], pool: DBConnection.ConnectionPool)
   end
@@ -31,23 +34,30 @@ defmodule Mix.Tasks.Seed do
       [],
       pool: DBConnection.ConnectionPool
     )
+
+    Postgrex.query!(
+      DB,
+      "Create TABLE users (id SERIAL, username VARCHAR(255) NOT NULL, password_hash CHAR(72) NOT NULL)",
+      [],
+      pool: DBConnection.ConnectionPool
+    )
   end
 
   defp seed_data() do
     IO.puts("Seeding data")
 
-    # Postgrex.query!(DB, "INSERT INTO fruits(order_id, completion, pizza_name, price, glutenfree, size, tomato, mozzarella, parmesan, pecorino, gorgonzola, ham, basil, mushroom, artichoke, olives, pepper, salami, aubergine, zucchini, chili) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)",
-    #   [1, 0, "Margherita", 85, false, 100, false, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    Postgrex.query!(DB, "INSERT INTO pizzas (order_id, completion, pizza_name, price, glutenfree, size, tomato, mozzarella, parmesan, pecorino, gorgonzola, ham, basil, mushroom, artichoke, olives, pepper, salami, aubergine, zucchini, chili) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)",
+      [1, 0, "Margherita", 85, false, false, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-    #   # pool: DBConnection.ConnectionPool
-    # )
+      pool: DBConnection.ConnectionPool
+    )
 
 
-    # Postgrex.query!(
-    #   DB,
-    #   "INSERT INTO users(username, password_hash) VALUES($1, $2)",
-    #   ["a", Bcrypt.hash_pwd_salt("a")],
-    #   pool: DBConnection.ConnectionPool
-    # )
+    Postgrex.query!(
+      DB,
+      "INSERT INTO users(username, password_hash) VALUES($1, $2)",
+      ["a", Bcrypt.hash_pwd_salt("a")],
+      pool: DBConnection.ConnectionPool
+    )
   end
 end
--- lib/pluggy/router.ex
+++ lib/pluggy/router.ex
@@ -2,7 +2,7 @@ defmodule Pluggy.Router do
   use Plug.Router
   use Plug.Debugger
 
-  alias Pluggy.FruitController
+  alias Pluggy.PizzaController
   alias Pluggy.UserController
 
   plug(Plug.Static, at: "/", from: :pluggy)
@@ -21,18 +21,18 @@ defmodule Pluggy.Router do
   plug(:match)
   plug(:dispatch)
 
-  get("/fruits", do: FruitController.index(conn))
-  get("/fruits/new", do: FruitController.new(conn))
-  get("/fruits/:id", do: FruitController.show(conn, id))
-  get("/fruits/:id/edit", do: FruitController.edit(conn, id))
+  get("/pizzas", do: PizzaController.index(conn))
+  get("/pizzas/new", do: PizzaController.new(conn))
+  get("/pizzas/:id", do: PizzaController.show(conn, id))
+  get("/pizzas/:id/edit", do: PizzaController.edit(conn, id))
 
-  post("/fruits", do: FruitController.create(conn, conn.body_params))
+  post("/pizzas", do: PizzaController.create(conn, conn.body_params))
 
-  # should be put /fruits/:id, but put/patch/delete are not supported without hidden inputs
-  post("/fruits/:id/edit", do: FruitController.update(conn, id, conn.body_params))
+  # should be put /pizzas/:id, but put/patch/delete are not supported without hidden inputs
+  post("/pizzas/:id/edit", do: PizzaController.update(conn, id, conn.body_params))
 
-  # should be delete /fruits/:id, but put/patch/delete are not supported without hidden inputs
-  post("/fruits/:id/destroy", do: FruitController.destroy(conn, id))
+  # should be delete /pizzas/:id, but put/patch/delete are not supported without hidden inputs
+  post("/pizzas/:id/destroy", do: PizzaController.destroy(conn, id))
 
   post("/users/login", do: UserController.login(conn, conn.body_params))
   post("/users/logout", do: UserController.logout(conn))
